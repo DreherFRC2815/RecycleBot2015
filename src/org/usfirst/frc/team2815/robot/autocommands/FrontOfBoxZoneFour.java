0bb98e44d658d36eb3ebd7984e7b3c3d30a77390
DreherFRC2815 <bluemevilmechanics@gmail.com>
2015-02-07 14:35:26 -0500

    Added code for the fourth zone

---
+++ src/org/usfirst/frc/team2815/robot/autocommands/FrontOfBoxZoneFour.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team2815.robot.autocommands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class FrontOfBoxZoneFour extends CommandGroup {
+    
+    public  FrontOfBoxZoneFour() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+    	addSequential(new DriveForward(2));
+    	addSequential(new IterativeDriveForward(3));
+    	addParallel(new RaiseElevator(2));
+    	addSequential(new TurnRight(4));
+    	addSequential(new LowerElevator(1.5));
+    	addSequential(new DriveBackward(2));
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
